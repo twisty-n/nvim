39384543f537d22ff662b797a8cf72124bc766bb
Tristan <tristan.newmann@appian.com>
2021-10-10 23:29:22 +1100

    Enable compatible colours. Install treesitter and telescope

--- init.lua
+++ init.lua
@@ -2,18 +2,15 @@ require "packages"
 
 --options vim.api.nvim_set_option
 vim.o.number = true
-vim.o.background = 'dark'
+--vim.o.background = 'dark'
 vim.o.laststatus = 2 -- always show statusbar
 vim.o.backup = true
-vim.go.t_Co = '256'
+--vim.go.t_Co = '256'
 vim.o.hlsearch = true
 
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
-
--- disable powerline as it is not working with neovim 0.5
-vim.g.powerline_loaded = 1
 
 if (vim.api.nvim_eval('has("multi_byte")')) then
   vim.o.encoding = 'utf-8'
@@ -21,7 +18,7 @@ if (vim.api.nvim_eval('has("multi_byte")')) then
   vim.g.fileencodings = 'ucs-bom,utf-8,latin1'
 end
 
-vim.o.termguicolors = true
+-- vim.o.termguicolors = true
 
 vim.api.nvim_exec([[
   " For all text files set 'textwidth' to 78 characters.
@@ -29,7 +26,7 @@ vim.api.nvim_exec([[
   filetype indent on
   filetype plugin on
   autocmd BufRead,BufWritePre *.sh normal gg=G
-  colorscheme dracula
+  colorscheme crayon
   setglobal autoindent smartindent
 ]], true)
 
--- lua/packages.lua
+++ lua/packages.lua
@@ -9,12 +9,40 @@ end
 require "paq" {
     "savq/paq-nvim";                  -- Let Paq manage itself
 
+    -- Search
+    { 'nvim-lua/popup.nvim', as='popup' };
+    { 'nvim-lua/plenary.nvim', as='plenary'};
+    { 'nvim-telescope/telescope.nvim', as='telescope' };
+
     -- Cosmetics
-    {"dracula/vim", as="dracula"};
-    {"mhinz/vim-startify", as="startify"};
-    { 'hoob3rt/lualine.nvim', as="lualine"};
-    { 'kyazdani42/nvim-web-devicons', as="nvim-web-devicons"};
-    { 'ryanoasis/vim-devicons', as="vim-devicons"};
+    { "dracula/vim", as="dracula" };
+    { "dylanaraps/crayon", as="crayon-scheme" };
+    { "mhinz/vim-startify", as="startify" };
+    { 'hoob3rt/lualine', as="lualine" };
+    { 'kyazdani42/nvim-web-devicons', as="nvim-web-devicons" };
+    { 'ryanoasis/vim-devicons', as="vim-devicons" };
+
+    -- Language Stuff
+    { 'nvim-treesitter/nvim-treesitter', run=':TSUpdate' };
 }
 
 require('lualine').setup()
+
+-- Treesitter Things
+local ts = require 'nvim-treesitter.configs'
+ts.setup {
+    ensure_installed = "all",
+
+    highlight = {
+        enable = true,
+        disable = {"php"}
+    },
+
+    incremental_selection = {
+        enable = true,
+    },
+
+    indent = {
+        enable = true,
+    },
+}
