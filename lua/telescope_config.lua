9d9872c56a1e20402a3038732915722cd4f4521a
Tristan Newmann <tristan.newmann@appian.com>
2021-10-13 13:19:23 +1100

    Fix telescope crash issues

---
+++ lua/telescope_config.lua
@@ -0,0 +1,32 @@
+local previewers = require('telescope.previewers')
+local Job = require('plenary.job')
+local new_maker = function(filepath, bufnr, opts)
+    filepath = vim.fn.expand(filepath)
+    Job:new({
+        command = 'file',
+        args = { '--mime-type', '-b', filepath },
+        on_exit = function(j)
+            local mime_type = vim.split(j:result()[1], '/')[1]
+            if mime_type == "text" then
+                previewers.buffer_previewer_maker(filepath, bufnr, opts)
+            else
+                -- maybe we want to write something to the buffer here
+                vim.schedule(function()
+                    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { [[Don't preview binary files]] })
+                end)
+            end
+        end
+    }):sync()
+end
+
+require('telescope').setup {
+    defaults = {
+        preview = {
+            check_mime_type = false
+        },
+        buffer_previewer_maker = new_maker,
+        file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+        grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+        qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+    }
+}
